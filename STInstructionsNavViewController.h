6013922f81903c4527757d9296c06de4e0282374
Richard Sprague <richardsprague@gmail.com>
2014-02-13 21:23:46 -0800

    Cleanup before starting branch

---
+++ STInstructionsNavViewController.h
@@ -0,0 +1,14 @@
+//
+//  STInstructionsNavViewController.h
+//  StroopTestNew
+//
+//  Created by Richard Sprague on 5/21/13.
+//  Copyright (c) 2013 Richard Sprague. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "STViewController.h"
+
+@interface STInstructionsNavViewController : UINavigationController
+
+@end
